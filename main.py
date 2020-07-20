eaa2e733809851c310c3c141028a9cd464319864
NateSpace <amh2os@yahoo.com>
2020-07-20 15:11:41 +0000

    I created a quiz

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Nate Is The Greatest Coder On Earth")
+# greet the User
+name = input ("enter your name:")
+print("hello"+name)
+name = input("Enter your hobby:" )
+print("your hobby is" +name )
+Snakes = {"AAA" : "green tree python",
+"AAB" : "rainbow boa",
+"ABA" : "Black mamba",
+"ABB" : "copperhead",
+"BAA" : "hairy bush viper",
+"BAB" : "death adder",
+"BBA" : "Inland Taipan",
+"BBB" : "yellow bellyed sea snake"}
+question1 = input("what would you rather play a) roblox or b) KSP:")
+question2 = input("which do you prefer a) red or b) blue:" )
+question3 = input("do you prefer a) siberian huskys or b) beagles:")
+print("your Sanke is " +Snakes[question1+question2+question3])
